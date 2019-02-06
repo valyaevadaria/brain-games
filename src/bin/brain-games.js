e477ba28c137acfcb1f8227efd166b3224b66479
Daria <valyaevadaria@gmail.com>
2019-02-06 07:27:45 +0700

    ask name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import askName from '..';
+
 console.log('Welcome to the Brain Games!');
+
+console.log('Hello, ' + askName());
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import readlineSynk from 'readline-sync';
+
+export default () => readlineSynk.question('May I have your name? ');
+
