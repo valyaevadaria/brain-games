1254508a9b09310c05ab9ab28c5931caa3246d06
Daria <valyaevadaria@gmail.com>
2019-02-07 23:01:36 +0700

    correction 4

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,35 +1,5 @@
-import readlineSynk from 'readline-sync';
+#!/usr/bin/env node
 
-const brainEven = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log(`Answer "yes" if number even otherwise answer "no".`);
-
-  const name = readlineSynk.question('May I have your name? ');
-  console.log('Hello, ' + name);
-
-  const number = Math.round(Math.random() * 100);
-  console.log('Question: ' + number);
-  const answer = readlineSynk.question('Your answer: ');
-
-  const iter = (n, a, step) => {
-    const correctAnswer = n%2 === 0 ? 'yes' : 'no';
-    if ((a !== 'yes' && a !== 'no') || a !== correctAnswer){
-      console.log(`'${a}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      return console.log(`Let's try again, ${name}!`);
-    }
-    if (a === correctAnswer){
-      console.log('Correct!');
-      const newNumber = Math.round(Math.random() * 100);
-      console.log('Question: ' + newNumber);
-      const newAnswer = readlineSynk.question('Your answer: ');
-      return iter(newNumber, newAnswer, step+1);
-    }
-    if (step === 4){
-      return console.log(`Congratulations, ${name}`);
-    }
-  }
-    
-  return iter(number, answer, 1);
-};
+import brainEven from '..';
 
 brainEven();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import readlineSynk from 'readline-sync';
+import isEven from './func/iseven';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+
   const name = readlineSynk.question('May I have your name? ');
-  console.log('Hello, ' + name);
-}
+  console.log(`Hello, ${name}`);
 
+  const iter = (raundOfGame) => {
+    const numberForQuewstion = Math.round(Math.random() * 100);
+    console.log(`Question: ${numberForQuewstion}`);
+    const userAnswer = readlineSynk.question('Your answer: ');
 
+    const correctAnswer = isEven(numberForQuewstion) ? 'yes' : 'no';
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      if (raundOfGame === 3) {
+        console.log(`Congratulations, ${name}!`);
+      } else {
+        iter(raundOfGame + 1);
+      }
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+    }
+  };
+  iter(1);
+};
