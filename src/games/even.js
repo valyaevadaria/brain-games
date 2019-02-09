210cf12f200fa53b3e23a3e16c6ab9ad8073d96c
Daria <valyaevadaria@gmail.com>
2019-02-10 02:01:37 +0700

    add brain-prime

--- src/games/even.js
+++ src/games/even.js
@@ -7,9 +7,9 @@ const isEven = number => (number % 2 === 0);
 const startMessage = 'Answer "yes" if number even otherwise answer "no".';
 
 const brainEven = () => {
-  const quewstion = getRandomNumber(100);
-  const answer = isEven(quewstion) ? 'yes' : 'no';
-  return cons(quewstion, answer);
+  const question = getRandomNumber(100);
+  const answer = isEven(question) ? 'yes' : 'no';
+  return cons(question, answer);
 };
 
 export default () => startGame(startMessage, brainEven, 3);
---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import startGame from '..';
+import { cons } from 'hexlet-pairs';
+import getRandomNumber from '../utils';
+
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  const iter = (divider) => {
+    if (number % divider === 0 && divider < number) {
+      return false;
+    } if (divider === number) {
+      return true;
+    }
+    return iter(divider + 1);
+  };
+  return iter(2);
+};
+
+const startMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const brainPrime = () => {
+  const question = getRandomNumber(100);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, answer);
+};
+
+export default () => startGame(startMessage, brainPrime, 3);
