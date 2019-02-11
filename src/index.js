1c92e4cb8afc95054f1478f85df335b24748badb
Daria Valyaeva <41881515+valyaevadaria@users.noreply.github.com>
2019-02-11 14:45:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,24 +10,23 @@ export default (message, game) => {
   const name = readlineSynk.question('May I have your name? ');
   console.log(`Hello, ${name}`);
 
-  const iter = (raundOfGame) => {
+   const iter = (raundOfGame) => {
     if (raundOfGame > countOfRaunds) {
       console.log(`Congratulations, ${name}!`);
-    } else {
-      const getDataForRaund = game();
-      const question = car(getDataForRaund);
-      const correctAnswer = cdr(getDataForRaund);
-
-      console.log(`Question: ${question}`);
-      const userAnswer = readlineSynk.question('Your answer: ');
-      if (userAnswer === correctAnswer) {
-        console.log('Correct!');
-        iter(raundOfGame + 1);
-      } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-        console.log(`Let's try again, ${name}!`);
-      }
+      return;
     }
-  };
+    const getDataForRaund = game();
+    const question = car(getDataForRaund);
+    const correctAnswer = cdr(getDataForRaund);
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSynk.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      iter(raundOfGame + 1);
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+    }
   iter(1);
 };
